7ed7aa518f706a7ba07f4948a033e362b76a1f99
Likhithap012 <likhithap012@gmail.com>
2025-05-07 12:00:00 +0530

    Added generics

--- src/com/gevernova/exceptionalhandling/InsufficientBalanceException.java
+++
@@ -1,7 +0,0 @@
-package com.gevernova.exceptionalhandling;
-
-public class InsufficientBalanceException extends Exception {
-    public InsufficientBalanceException(String message){
-        super(message);
-    }
-}
